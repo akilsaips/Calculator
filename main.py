f3d1eb4e966c2488d1d50349835ec33a17ae8e8b
Akilsai <86817062+akilsaips@users.noreply.github.com>
2023-10-31 22:58:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+print(logo)
+
+def add(n1,n2):
+  return n1+n2
+
+def subtract(n1,n2):
+  return n1-n2
+
+def multiply(n1,n2):
+  return n1*n2
+
+def divide(n1,n2):
+  return n1/n2
+
+operation = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+
+def calculator():
+    num1 = float(input("Enter the first number? "))
+
+    for symbol in operation:
+        print(symbol)
+
+    shouldcontinue = True
+
+    while shouldcontinue:
+        pick_operation = input("Pick an operation: ")
+
+        num2 = float(input("Enter the next number: "))
+
+        chose_operation = operation[pick_operation]
+
+        answer = chose_operation(num1,num2)
+
+        print(f"{num1}{pick_operation}{num2} = {answer}")
+
+        resume = input("Enter y to contine, n to start a fresh calculation: or press exit to quit. ").lower()
+
+        if resume == "y":
+            num1 = answer
+        elif resume == "n":
+            shouldcontinue = False
+            calculator()
+        else:
+          print("Thanks for calculating!")
+          break
+
+
+calculator()
